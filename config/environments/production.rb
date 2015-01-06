245ef847e7ca979b61841865afd6458e6460e170
Jeffrey Wan <Jwan622@gmail.com>
2015-01-06 14:30:51 -0700

    generated people model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  #serve static assets
+  config.serve_static_assets = true
 end
