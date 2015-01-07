6950224121a001b2e880505f9ccade3e521b010a
Jeffrey Wan <Jwan622@gmail.com>
2015-01-06 22:28:12 -0700

    phone_number_controller tests added

--- db/migrate/20150107000502_create_phone_numbers.rb
+++ db/migrate/20150107000502_create_phone_numbers.rb
@@ -4,7 +4,7 @@ class CreatePhoneNumbers < ActiveRecord::Migration
       t.string :number
       t.integer :person_id
 
-      t.timestamps null: false
+      t.timestamps null: false   #if you try to insert into a database, it will kick it back. It's a database-side validation. I can't insert into it without timestamps. 
     end
   end
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: "2123630854", person_id: 1) }
+
+
+  it 'is valid' do
+    expect(phone_number).to be_valid   #phone_number is an object. It's an object that passes the validate
+  end
+
+  it 'is invalid without a number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
 end
